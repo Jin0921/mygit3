d2510891eae3c976a731e0fc628eccd88c25729b
Jin0921 <18749855536@163.com>
2021-08-31 15:48:33 +0800

    第一次提交

---
+++ gitTest/src/com/git/gitTest1.java
@@ -0,0 +1,14 @@
+package com.git;
+
+
+public class gitTest1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+
+}
